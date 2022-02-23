05147d2905cca734ecec47639bb8e629fc7e772e
Aleksandr <dentranscom@gmail.com>
2022-02-23 11:24:39 +0200

    feat: add accordion

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,37 @@
+const accordion = (triggersSelector) => {
+    const btns = document.querySelectorAll(triggersSelector);
+
+    // С помощью JS
+    btns.forEach(btn => {
+        btn.addEventListener('click', function() {
+            this.classList.toggle('active-style');
+            this.nextElementSibling.classList.toggle('active-content');
+
+            if (this.classList.contains('active-style')) {
+                this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
+            } else {
+                this.nextElementSibling.style.maxHeight = '0px';
+            }
+        });
+    });
+
+    // С помощью стилей
+    //       blocks = document.querySelectorAll(itemsSelector);
+
+    // blocks.forEach(block => {
+    //     block.classList.add('animated', 'fadeInDown');
+    // });
+
+    // btns.forEach(btn => {
+    //     btn.addEventListener('click', function() { //Если у елемерта по которому кликнули нет класса активности,
+    //         if (!this.classList.contains('active')) { // то мы ему добавим класс активности а у остальных уберем. 
+    //             btns.forEach(btn => {
+    //                 btn.classList.remove('active', 'active-style');
+    //             });
+    //             this.classList.add('active', 'active-style');
+    //         }
+    //     });
+    // });
+};
+
+export default accordion;
